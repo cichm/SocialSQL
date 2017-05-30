087e82a66cfa8c348549c539b68631a2162da666
Mieszko Cichon <svider.mc@gmail.com>
2017-05-30 13:08:03 +0200

    Hello World!

--- SocialNetwork.sql
+++ SocialNetwork.sql
@@ -30,13 +30,13 @@
 -- Dumping data for table 'is_admin'
 --
 CREATE TABLE users (
-  user_id 		  NUMBER(7) 	NOT NULL CONSTRAINT user_id_us PRIMARY KEY ,
-  Name 			    varchar(25) NOT NULL,
-  Email 			  varchar(30) NOT NULL,
-  Passwd 		  varchar(30) NOT NULL,
-  Gender 			  varchar(6) 	NOT NULL,
-  Birthday_Date varchar(11) NOT NULL,
-  FB_Join_Date 	varchar(30) NOT NULL
+  user_id			NUMBER(7) 	NOT NULL CONSTRAINT user_id_us PRIMARY KEY ,
+  Name 				varchar(25) NOT NULL,
+  Email				varchar(30) NOT NULL,
+  Passwd			varchar(30) NOT NULL,
+  Gender 			varchar(6) 	NOT NULL,
+  Birthday_Date 	varchar(11) NOT NULL,
+  FB_Join_Date 		varchar(30) NOT NULL
 );
 
 INSERT INTO users (user_id, Name, Email, Passwd, Gender, Birthday_Date, FB_Join_Date) VALUES
@@ -86,11 +86,11 @@ INSERT INTO feedback (feedback_id, user_id, feedback_txt, star, Date_info) VALUE
 (10, 1, 'Good book', '4', '20-12-2015 16:59');
 
 CREATE TABLE user_info (
-  user_id 		  NUMBER(7)	    NOT NULL CONSTRAINT user_id_us_in REFERENCES users(user_id),
-  job 			    varchar(100),
+  user_id		NUMBER(7)	    NOT NULL CONSTRAINT user_id_us_in REFERENCES users(user_id),
+  job			varchar(100),
   current_city 	varchar(100) 	NOT NULL,
-  hometown 		  varchar(100) 	NOT NULL,
-  website 		  varchar(100),
+  hometown 		varchar(100) 	NOT NULL,
+  website 		varchar(100),
   Facebook_ID 	VARCHAR(100) 	NOT NULL
 );
 
@@ -114,8 +114,8 @@ INSERT INTO user_info (user_id, job, current_city, hometown, website, Facebook_I
 (9, '', 'Krakow', 'Rzeszow', '', 'www.facebook.com/dasdas3.0006543');
 
 CREATE TABLE user_post (
-  post_id NUMBER(7) 			NOT NULL CONSTRAINT post_id_us    PRIMARY KEY,
-  user_id NUMBER(7) 			NOT NULL CONSTRAINT user_id_us_po REFERENCES users(user_id),
+  post_id NUMBER(7) 		NOT NULL CONSTRAINT post_id_us    PRIMARY KEY,
+  user_id NUMBER(7) 		NOT NULL CONSTRAINT user_id_us_po REFERENCES users(user_id),
   post_txt VARCHAR(250) 	NOT NULL,
   post_time varchar(30) 	NOT NULL
 );
@@ -128,10 +128,10 @@ INSERT INTO user_post (post_id, user_id, post_txt, post_time) VALUES
 (3, 3, 'Welcome!', '18-10-2006 12:10');
 
 CREATE TABLE user_post_comment (
-  comment_id    NUMBER(7) 	  NOT NULL CONSTRAINT comment_id_us    PRIMARY KEY,
-  post_id       NUMBER(7)    NOT NULL CONSTRAINT post_id_us_po_co  REFERENCES user_post(post_id),
-  user_id       NUMBER(7)    NOT NULL CONSTRAINT user_id_us_po_co  REFERENCES users(user_id),
-  comment_text  VARCHAR(250)  NOT NULL
+  comment_id    NUMBER(7)		NOT NULL CONSTRAINT comment_id_us    PRIMARY KEY,
+  post_id       NUMBER(7)		NOT NULL CONSTRAINT post_id_us_po_co  REFERENCES user_post(post_id),
+  user_id       NUMBER(7)		NOT NULL CONSTRAINT user_id_us_po_co  REFERENCES users(user_id),
+  comment_text  VARCHAR(250)	NOT NULL
 );
 
 INSERT INTO user_post_comment (comment_id, post_id, user_id, comment_text) VALUES
@@ -142,8 +142,8 @@ INSERT INTO user_post_comment (comment_id, post_id, user_id, comment_text) VALUE
 (3, 3, 3, 'That is right');
 
 CREATE TABLE user_lock (
-  user_id   NUMBER(7) 	  NOT NULL CONSTRAINT user_id_us_lo REFERENCES users(user_id),
-  user_ban  NUMBER(1,0) 	NOT NULL
+  user_id   NUMBER(7) 	NOT NULL CONSTRAINT user_id_us_lo REFERENCES users(user_id),
+  user_ban  NUMBER(1,0) NOT NULL
 );
 
 INSERT INTO user_lock (user_id, user_ban) VALUES
@@ -175,8 +175,8 @@ INSERT INTO user_name_pass (user_id, Username, Passwd) VALUES
 (9, 'wentek', 'someInfoPasswd');
 
 CREATE TABLE is_admin (
-  user_id 	NUMBER(7)   NOT NULL CONSTRAINT user_id_us_admin REFERENCES users(user_id),
-  is_admin   NUMBER(1,0) NOT NULL
+  user_id 	NUMBER(7)   	NOT NULL CONSTRAINT user_id_us_admin REFERENCES users(user_id),
+  is_admin   NUMBER(1,0) 	NOT NULL
 );
 
 INSERT INTO is_admin (user_id, is_admin) VALUES
